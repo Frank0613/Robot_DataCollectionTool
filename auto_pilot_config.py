6d4c8006cee9734d5fb60698ea228c0b695abc03
Frank0613 <frankyen0613@gmail.com>
2026-01-17 12:31:47 +0800

    Auto pilot for cube (all step)

--- auto_pilot_config.py
+++ auto_pilot_config.py
@@ -2,7 +2,8 @@ GRASP_SEQUENCE = [
 
     # Step 0: (Move to top of target pos)
     {
-        "name": "Descend", 
+        "name": "Descend",
+        "target": "cube", 
         "offset": [0.0, 0.0, 0.1], 
         "tol": 0.11, 
         "gripper": 1, 
@@ -10,7 +11,8 @@ GRASP_SEQUENCE = [
     },
     # Step 1: (Move to target pos)
     {
-        "name": "Descend", 
+        "name": "Descend",
+        "target": "cube", 
         "offset": [0.0, 0.0, 0.00], 
         "tol": 0.11, 
         "gripper": 1, 
@@ -18,7 +20,8 @@ GRASP_SEQUENCE = [
     },
     # Step 2: (Close gripper)
     {
-        "name": "Grasp", 
+        "name": "Grasp",
+        "target": "cube", 
         "offset": [0.0, 0.0, 0.00], 
         "tol": 0.11, 
         "gripper": -1, 
@@ -26,10 +29,47 @@ GRASP_SEQUENCE = [
     },
     # Step 3: (Lift up)
     {
-        "name": "Lift", 
+        "name": "Lift",
+        "target": "cube", 
         "offset": [0.0, 0.0, 0.15], 
         "tol": 0.15, 
         "gripper": -1, 
+        "wait_frames": 120
+    },
+    # Step 4: (Top of container)
+    {
+        "name": "MoveToContainer", 
+        "target": "container",  
+        "offset": [0.0, 0.0, 0.20], 
+        "tol": 0.15, 
+        "gripper": -1, 
+        "wait_frames": 40
+    },
+    # Step 5: (Descend)
+    {
+        "name": "DescendToDrop", 
+        "target": "container",
+        "offset": [0.0, 0.0, 0.10], # 進入籃子上方一點點
+        "tol": 0.15, 
+        "gripper": -1, 
+        "wait_frames": 30
+    },
+    # Step 6: (Release)
+    {
+        "name": "Release", 
+        "target": "container",
+        "offset": [0.0, 0.0, 0.05], 
+        "tol": 0.15, 
+        "gripper": 1, 
+        "wait_frames": 60
+    },
+    # Step 7: (Lift)
+    {
+        "name": "PostReleaseLift", 
+        "target": "container",
+        "offset": [0.0, 0.0, 0.25], 
+        "tol": 0.15, 
+        "gripper": 1, 
         "wait_frames": 120
     }
 ]
